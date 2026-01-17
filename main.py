53a589a1588845ff4411614817e1b7ef36388cdd
Alex <byteforge650@gmail.com>
2026-01-16 23:29:43 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+def open_calculator():
+    print("Welcome to the calculator!\n")
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    operator = input("Select an operation (+, -, * or /): ")
+
+    if operator == "+":
+        value = num1 + num2
+        print("Answer: " + str(value))
+    elif operator == "-":
+        value = num1 + num2
+        print("Answer: " + str(value))
+    elif operator == "*":
+        value = num1 + num2
+        print("Answer: " + str(value))
+    elif operator == "/":
+        if num2 == 0:
+            print("You cannot devide by 0!")
+            open_calculator()
+        else:
+            value = num1 / num2
+            print("Answer: " + str(value))
+
+open_calculator()
