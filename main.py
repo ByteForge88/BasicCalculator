4730e9d18a49c3b5049f08951744b06a52bbef43
Alex <byteforge650@gmail.com>
2026-01-17 17:02:49 -0800

    Better!

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 def open_calculator():
-    print("Welcome to the calculator!\n")
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter the second number: "))
-    operator = input("Select an operation (+, -, * or /): ")
+    operator = input("Select an operator (+, -, * or /): ")
 
     if operator == "+":
-        value = num1 + num2
-        print("Answer: " + str(value))
+        result = num1 + num2
+        print("\nAnswer: " + str(result) + "\n")
+        open()
     elif operator == "-":
-        value = num1 - num2
-        print("Answer: " + str(value))
+        result = num1 - num2
+        print("\nAnswer: " + str(result) + "\n")
+        open()
     elif operator == "*":
-        value = num1 * num2
-        print("Answer: " + str(value))
+        result = num1 * num2
+        print("\nAnswer: " + str(result) + "\n")
+        open()
     elif operator == "/":
         if num2 == 0:
             print("You cannot divide by 0!")
-            open_calculator()
+            open()
         else:
-            value = num1 / num2
-            print("Answer: " + str(value))
+            result = num1 / num2
+            print("\nAnswer: " + str(result) + "\n")
+            open()
 
-open_calculator()
+def open():
+    print("Welcome to the calculator!\n")
+    user_input = input("Press 'c' to contiue or press 'e' to exit: ")
+
+    if user_input == "c":
+        open_calculator()
+    elif user_input == "e":
+        print("Goodbye!")
+
+open()
